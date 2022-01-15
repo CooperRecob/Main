c342f27332c347904ea7c29560d0102ffbdd400f
Cooper Recob <crecob1@gmail.com>
2022-01-14 22:27:24 -0600

    update

---
+++ apcs/unit6/enhancedForLoop/Animal.java
@@ -0,0 +1,17 @@
+package enhancedForLoop;
+
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public void setAnimal(String newName) {
+        name = newName;
+    }
+}
