af93f56250a46e00f446b450aa5e987cd03e3f99
Cooper Recob <crecob1@gmail.com>
2022-01-27 19:57:30 -0600

    update

---
+++ apcs/unit7/arrayListNotes/MyOwnThing.java
@@ -0,0 +1,11 @@
+package apcs.unit7.arrayListNotes;
+import java.util.ArrayList;
+
+public class MyOwnThing {
+    public static void main(String[] args) {
+        ArrayList<Integer> test = new ArrayList<>();
+
+        test.add(10);
+        System.out.println(test.get(0));
+    }
+}
