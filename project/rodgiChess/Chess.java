9994c44f1397abaf6223f1cc175b02b9d57a6d16
Cooper Recob <crecob1@gmail.com>
2022-01-31 13:44:28 -0600

    update

--- project/rodgiChess/Chess.java
+++
@@ -1,5 +0,0 @@
-package project.rodgiChess;
-
-public class Chess {
-    
-}
--- project/rodgiChess/Main.java
+++
@@ -1,7 +0,0 @@
-package project.rodgiChess;
-
-public class Main {
-    public static void main(String[] args) {
-        
-    }
-}
