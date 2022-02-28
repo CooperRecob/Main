aa927fe4a3b80c462e429f5ae0a1cd1d697e0904
Cooper Recob <crecob1@gmail.com>
2022-02-28 09:24:03 -0600

    update

--- com/project/battleship/Board.java
+++ com/project/battleship/Board.java
@@ -4,17 +4,10 @@ public class Board {
      private Ship[][] board;
 
      public Board(Ship ship1, Ship ship2, Ship ship3) {
+          board = new Ship[8][8];
           board[ship1.getX()][ship1.getY()] = ship1;
           board[ship2.getX()][ship2.getY()] = ship2;
           board[ship3.getX()][ship3.getY()] = ship3;
-
-          for(int x = 0; x < 8; x++) {
-               for(int y = 0; y < 8; y++) {
-                    if(board[x][y] == null) {
-                         board[x][y] = null;
-                    }
-               }
-          }
      }
 
      public int hitCount() {
@@ -22,7 +15,7 @@ public class Board {
 
           for(int x = 0; x < 8; x++) {
                for(int y = 0; y < 8; y++) {
-                    if(board[x][y] != null) {
+                    if(board[x][y] != null && board[x][y].getHit() == true) {
                          result++;
                     }
                }
@@ -30,9 +23,11 @@ public class Board {
           return result;
      }
 
-     public void shot(int x, int y) {
+     public boolean shot(int x, int y) {
           if(board[x][y] != null) {
                board[x][y].setHit(true);
+               return true;
           }
+          return false;
      }
 }
