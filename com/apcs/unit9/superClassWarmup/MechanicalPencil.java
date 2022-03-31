5fc20814b82fa4efda8c5fdeeb7e15d1ec727025
Cooper Recob <crecob1@gmail.com>
2022-03-31 15:06:31 -0500

    update

---
+++ com/apcs/unit9/superClassWarmup/MechanicalPencil.java
@@ -0,0 +1,19 @@
+package com.apcs.unit9.superClassWarmup;
+
+public class MechanicalPencil extends Pencil {
+    //private double cost;
+
+    public MechanicalPencil(boolean isErasable, int amountInkOrLead, boolean isNum2, double cost) {
+        super(isErasable, amountInkOrLead, isNum2);
+        //this.cost = cost;
+    }
+
+    public void refilLead(int peices) {
+        if(peices > 0) {
+            setAmountInkOrLead(getAmountInkOrLead() + peices);
+            System.out.println("The mechanical pencil refilled " + peices + " peices of lead");
+        } else {
+            System.out.println("You didn't put any in");
+        }
+    }
+}
