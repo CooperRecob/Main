6a93cbd0e29491782c7431af746c7df95a002f99
Cooper Recob <crecob1@gmail.com>
2022-04-12 14:56:30 -0500

    update

---
+++ com/apcs/unit9/vehicleTest/MomVan.java
@@ -0,0 +1,14 @@
+package com.apcs.unit9.vehicleTest;
+
+public class MomVan extends Van {
+    private int children;
+
+    public MomVan(String color, String manufacturer, int seats, int children) {
+        super(color, manufacturer, seats);
+        this.children = children;
+    }
+
+    public void Drive() {
+        System.out.println("Driving a " + getManufacturer() + " van with " + getSeats() + " seats and " + children + " children.");
+    }
+}
