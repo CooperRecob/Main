31c1e1142e40f80bb30b511a0d62d121170159bb
Cooper Recob <crecob1@gmail.com>
2022-06-03 13:59:00 -0500

    update

---
+++ bin/tests/python/sus.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+for x in range(4):
+    if x == 3:
+        print("Cool")
+    else:
+        print("Not Cool")
+
+for y in range(7):
+    if x < 3:
+        print("x < 3")
+    else:
+        print("x >= 3")
