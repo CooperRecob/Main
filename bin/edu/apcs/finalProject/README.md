8c21c19d9e96c3438b8cfb3d1a0637c2fe13ac3c
Cooper Recob <crecob1@gmail.com>
2022-06-03 13:48:10 -0500

    update

--- bin/edu/apcs/finalProject/README.md
+++ bin/edu/apcs/finalProject/README.md
@@ -1,2 +1,5 @@
-## Final Project for APCS
-My final for apcs is a website, located at cooperrecob.com
+## Sudoku Solver
+This sudoku solver uses the popular but slow backtracking
+algorithm to solve sudoku puzzles, there is an example
+puzzle included but you can change the vaules of the 
+input puzzle in any way you would want
---
+++ edu/apcs/finalProject/Main.java
@@ -0,0 +1,31 @@
+package edu.apcs.finalProject;
+
+public class Main {
+    public static void main(String[] args) {
+        //create a new valid starting sudoku board
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+        SudokuSolver solver = new SudokuSolver(board);
+        int[][] solution = solver.solve();
+        if(solution != null){
+            System.out.println("Solution found!");
+            for(int i = 0; i < 9; i++){
+                for(int j = 0; j < 9; j++){
+                    System.out.print(solution[i][j] + " ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("No solution found!");
+        }
+    }
+}
