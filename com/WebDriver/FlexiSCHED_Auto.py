14a8cbb99ca4f93a937363a24f6fef9be1aad3c4
Cooper Recob <crecob1@gmail.com>
2022-08-17 20:51:49 -0500

    update

--- com/WebDriver/FlexiSCHED_Auto.py
+++ com/WebDriver/FlexiSCHED_Auto.py
@@ -3,13 +3,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
 
+# read the username and password from the text file
+file = open("schedule.txt", "r")
+email = file.readline().rstrip()
+password = file.readline().rstrip()
+file.close()
+
+# inputs the username, password, and classes to be scheduled into the FlexiSCHED website
 driver = webdriver.Chrome()
 
 driver.get("https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?response_type=code&redirect_uri=https%3A%2F%2Ffortschools.flexisched.net%2FgoogleLogin.php&client_id=418948195496-fvrrvu52kn6rlcvhvn5td7t48i784vmm.apps.googleusercontent.com&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&access_type=online&approval_prompt=auto&flowName=GeneralOAuthFlow")
-
-# send email and password to the google login page
-email = "recocoo@fortschools.org"
-password = "K12447y1"
 
 email_input = driver.find_element(By.XPATH, "//input[@id='identifierId']")
 email_input.send_keys(email + Keys.RETURN)
---
+++ com/WebDriver/setup.py
@@ -0,0 +1,57 @@
+import tkinter
+from tkinter import ttk
+
+# use tkinter to get user input
+window = tkinter.Tk()
+window.title("FlexiSCHED Scheduler")
+window.geometry('300x200')
+
+def newSched():
+    window.destroy()
+
+    window2 = tkinter.Tk()
+    window2.title("FlexiSCHED Scheduler")
+
+    # get user input for new schedule
+    tkinter.Label(text="Enter Details for Schedule Below").grid(row=0, column=1, columnspan=3)
+
+    tkinter.Label(text="Monday").grid(row=1, column=0)
+    tkinter.Label(text="Tuesday").grid(row=1, column=1)
+    tkinter.Label(text="Wednesday").grid(row=1, column=2)
+    tkinter.Label(text="Thursday").grid(row=1, column=3)
+    tkinter.Label(text="Friday").grid(row=1, column=4)
+
+    tlist = ["default",
+             "Sarbacker"]
+
+    var, var2, var3, var4, var5 = tkinter.StringVar(), tkinter.StringVar(), tkinter.StringVar(), tkinter.StringVar(), tkinter.StringVar()
+
+    ttk.OptionMenu(window2, var, "default", *tlist).grid(row=2, column=0)
+    ttk.OptionMenu(window2, var2, "default", *tlist).grid(row=2, column=1)
+    ttk.OptionMenu(window2, var3, "default", *tlist).grid(row=2, column=2)
+    ttk.OptionMenu(window2, var4, "default", *tlist).grid(row=2, column=3)
+    ttk.OptionMenu(window2, var5, "default", *tlist).grid(row=2, column=4)
+
+    def save():
+        # save schedule to the directory
+        file = open("schedule.txt", "w")
+        file.write(email.get() + "\n" + password.get() + "\n")
+        file.write(var.get() + "\n" + var2.get() + "\n" + var3.get() + "\n" + var4.get() + "\n" + var5.get())
+        file.close()
+        window2.destroy()
+
+    tkinter.Button(text="Submit", command=lambda: save()).grid(row=3, column=2)
+
+    window2.mainloop()
+
+email = tkinter.StringVar()
+password = tkinter.StringVar()
+
+tkinter.Label(text="Welcome to FlexiSCHED Scheduler").pack()
+tkinter.Label(text="").pack()
+tkinter.Label(text="Please input your email and password below").pack()
+tkinter.Entry(textvariable=email).pack()
+tkinter.Entry(textvariable=password, show="*").pack()
+tkinter.Button(text="Submit", command=lambda: newSched()).pack()
+
+window.mainloop()
