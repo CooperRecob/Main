e7fe8bccaba6d6d3084deacd91aa83c70de71cc0
Cooper Recob <crecob1@gmail.com>
2022-09-22 16:57:53 +0000

    updated practice2 file to use printf

--- edu/cs220/practices/practice2.java
+++ edu/cs220/practices/practice2.java
@@ -3,24 +3,14 @@ package edu.cs220.practices;
 import java.util.Scanner;
 
 public class practice2 {
-    Scanner input = new Scanner(System.in);
-
-    //get the average of 2 inputed numbers and print it
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter a number: ");
-        int num1 = input.nextInt();
-
-        System.out.println("Enter another number: ");
-        int num2 = input.nextInt();
-
-        int sum = num1 + num2;
-        System.out.println("The sum of the two numbers is: " + sum);
-
-        double average = (double) sum / 2;
-        System.out.println("The average of the two numbers is: " + average);
-
+        int x = input.nextInt();
+        int y = input.nextInt();
+        int sum = x + y;
+        double avg = (double) sum / 2;
+        System.out.println("The sum is " + sum + " and the average is " + avg);
+        System.out.printf("The sum is %d and the average is %.2f\n", sum, avg);
         input.close();
     }
 }
