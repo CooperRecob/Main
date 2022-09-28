00ca2d60acf575baef5ff862d1c96360834173a7
Cooper Recob <crecob1@gmail.com>
2022-09-28 16:19:39 +0000

    takeHomeLab update

--- edu/uww/cs220/takeHomeLabs/homeLab2/BarChart.java
+++ edu/uww/cs220/takeHomeLabs/homeLab2/BarChart.java
@@ -8,8 +8,15 @@ public class BarChart {
     // opens salesData.txt and reads the data into an array with a length of the number of lines in the file
     // prints a bar graph of the data where each "*" represents $100 to the console and to a file called barChart.txt
     public static void main(String[] args) {
+        Scanner textInput = new Scanner(System.in);
+
+        System.out.print("Enter the name of the file: ");
+        String fileName = textInput.nextLine();
+        
+        textInput.close();
+
         try {
-            File file = new File("edu/uww/cs220/takeHomeLabs/lab2/salesData.txt");
+            File file = new File("edu/uww/cs220/takeHomeLabs/homeLab2/" + fileName);
             Scanner input = new Scanner(file);
             int[] data = new int[0];
             while (input.hasNextLine()) {
@@ -21,7 +28,7 @@ public class BarChart {
                 data = temp;
             }
             input.close();
-            FileWriter writer = new FileWriter("edu/uww/cs220/takeHomeLabs/lab2/SalesBarChart.txt");
+            FileWriter writer = new FileWriter("edu/uww/cs220/takeHomeLabs/homeLab2/SalesBarChart.txt");
             for (int i = 0; i < data.length; i++) {
                 System.out.print("Store " + (i + 1) + ": ");
                 writer.write("Store " + (i + 1) + ": ");
---
+++ edu/uww/cs220/takeHomeLabs/homeLab3/Sort2DArrayV2.java
@@ -0,0 +1,82 @@
+package edu.uww.cs220.takeHomeLabs.homeLab3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Sort2DArrayV2 {
+    // sort 3-by-3 matrix. Write a java program to sort the rows and columns in a
+    // 3-by-3 matrix. and then display both unsorted matrix and sorted matrixes
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("edu/uww/cs220/takeHomeLabs/homeLab3/ArrayData.txt"));
+            double[][] matrix = new double[3][3];
+            for (int i = 0; i < matrix.length; i++) {
+                String row = input.nextLine();
+                String[] rowArray = row.split(" ");
+                for (int j = 0; j < matrix[i].length; j++) {
+                    matrix[i][j] = Double.parseDouble(rowArray[j]);
+                }
+            }
+
+            // display unsorted matrix
+            System.out.println("The unsorted array is: ");
+            displayMatrix(matrix);
+
+            // only sort rows and print sorted rows
+            double[][] sortedRows = sortRows(matrix);
+
+            System.out.println("The row-sorted array is: ");
+            displayMatrix(sortedRows);
+
+            // only sort columns and print sorted columns
+            double[][] sortedColumns = sortColumns(matrix);
+
+            System.out.println("The column-sorted array is: ");
+            displayMatrix(sortedColumns);
+
+            input.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        };
+    }
+
+    private static void displayMatrix(double[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "|");
+            }
+            System.out.println();
+        }
+    }
+
+    private static double[][] sortRows(double[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                for (int k = 0; k < m[i].length - 1; k++) {
+                    if (m[i][k] > m[i][k + 1]) {
+                        double temp = m[i][k];
+                        m[i][k] = m[i][k + 1];
+                        m[i][k + 1] = temp;
+                    }
+                }
+            }
+        }
+        return m;
+    }
+
+    private static double[][] sortColumns(double[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                for (int k = 0; k < m[i].length - 1; k++) {
+                    if (m[k][i] > m[k + 1][i]) {
+                        double temp = m[k][i];
+                        m[k][i] = m[k + 1][i];
+                        m[k + 1][i] = temp;
+                    }
+                }
+            }
+        }
+        return m;
+    }
+}
