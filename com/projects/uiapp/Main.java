791cda2c835abb6d9dde34d2a5db09600b3a674e
Cooper Recob <crecob1@gmail.com>
2022-10-13 11:27:22 -0500

    test out some stuff in uiapp

--- com/projects/uiapp/Main.java
+++ com/projects/uiapp/Main.java
@@ -5,12 +5,23 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setSize(800, 800);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("My First Swing App");
-        frame.setVisible(true);
 
-        JLabel label = new JLabel("Hello World");
-        frame.add(label);
+        // Create a button that makes a line of text appear
+        JButton button = new JButton("Click me");
+        button.addActionListener(e -> {
+            JLabel label = new JLabel("Hello World");
+            frame.add(label);
+            frame.revalidate();
+        });
+        button.setBounds(100, 100, 100, 100);
+
+        // Add the button to the frame
+        frame.add(button);
+
+        // Set the frame's size and make it visible
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
