810d66eb63af569290a5acd9ba5e05c2ab50c862
Cooper Recob <crecob1@gmail.com>
2022-12-05 18:26:51 -0600

    update to hangman game to meet strech goals and finishing touches

--- edu/uww/cs220/project2/Main.java
+++ edu/uww/cs220/project2/Main.java
@@ -8,95 +8,87 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        FileInputStream fileByteStream = new FileInputStream("edu/uww/cs220/project2/WordBank.txt");
-        Scanner inFS = new Scanner(fileByteStream);
-        ArrayList<String> wordList = new ArrayList<>();
-        int count = 0;
-        while (inFS.hasNext()) {
-            wordList.add(count, inFS.next());
-            count++;
-        }
-        String theWord = wordList.get((int) (Math.random() * wordList.size()));
-       
+        try {
+            FileInputStream fileByteStream = new FileInputStream("edu/uww/cs220/project2/WordBank.txt"); // links to a file that contains all the words used for the game
+            Scanner file = new Scanner(fileByteStream);
+            ArrayList<String> wordList = new ArrayList<>(); // all the words from the word bank file get added to this listarray
+            int count = 0;
+            while (file.hasNext()) { // while there is more to read
+                wordList.add(count, file.next()); // add the next word to the list
+                count++;
+            }
+            Dice dice = new Dice(wordList.size() - 1); // create a new dice object with the number of sides equal to the number of words in the list to be rolled when the word is chosen
+            String theWord = wordList.get(dice.roll());
 
-        // instead of printing the word print the word as _ so if word is hello it will
-        // print _ _ _ _ _
-        // use the word.length() to determine how many _ to print
-        // but once you get a letter correct print the letter in the correct spot
-        // so if the user guesses h then it will print h _ _ _ _
-        // if the user guesses l then it will print h _ l l _
-        // once user gets all the letters correct then print the word and say you won
-        // if user runs out of guesses then print the word and say you lost
-
-        String theWordToGuess = "";
-        for (int i = 0; i < theWord.length(); i++) {
-            theWordToGuess += "_";
-        }
-
-        Scanner input = new Scanner(System.in);
-        int guesses = 0;
-
-        while (guesses < 6) {
-            System.out.println("Guess a letter");
-            char letter = input.next().charAt(0);
-            String temp = theWordToGuess;
+            String theWordToGuess = "";
             for (int i = 0; i < theWord.length(); i++) {
-                if (letter == theWord.charAt(i)) {
-                    theWordToGuess = theWordToGuess.substring(0, i) + letter + theWordToGuess.substring(i + 1);
-                }
+                theWordToGuess += "_";
             }
-            if (temp.equals(theWordToGuess)) {
-                guesses++;
-                // unbuild the hangman based on the number of guesses
-                // if guesses is 0 then print everything
-                // if guesses is 1 then print everything but the right leg
-                System.out.println("You have " + (6 - guesses) + " guesses left");
-                System.out.println(" _____________");
-		        System.out.println(" |          ||");
-		        System.out.println(" |          ||");
-                if (guesses == 0) {
-		            System.out.println(" O          ||");
-		            System.out.println("(|)         ||");
-		            System.out.println("( )         ||");
-                } else if (guesses == 1) {
-                    System.out.println(" O          ||");
-                    System.out.println("(|)         ||");
-                    System.out.println("  )         ||");
-                } else if (guesses == 2) {
-                    System.out.println(" O          ||");
-                    System.out.println("(|)         ||");
-                    System.out.println("            ||");
-                } else if (guesses == 3) {
-                    System.out.println(" O          ||");
-                    System.out.println(" |)         ||");
-                    System.out.println("            ||");
-                } else if (guesses == 4) {
-                    System.out.println(" O          ||");
+
+            Scanner input = new Scanner(System.in);
+            int guesses = 0;
+
+            while (guesses < 6) {
+                System.out.println("Guess a letter");
+                // takes the input from the user (doesnt matter if its a letter or not)
+                // if its not a letter then it will still count it as a guess
+                char letter = input.next().charAt(0);
+                String temp = theWordToGuess;
+                // makes the word to guess show correct letters
+                for (int i = 0; i < theWord.length(); i++) {
+                    if (letter == theWord.charAt(i)) {
+                        theWordToGuess = theWordToGuess.substring(0, i) + letter + theWordToGuess.substring(i + 1);
+                    }
+                }
+                // unbuilds the hangman as the user guesses wrong
+                if (temp.equals(theWordToGuess)) {
+                    guesses++;
+                    System.out.println("You have " + (6 - guesses) + " guesses left");
+                    System.out.println(" _____________");
                     System.out.println(" |          ||");
+                    System.out.println(" |          ||");
+                    if (guesses == 0) {
+                        System.out.println(" O          ||");
+                        System.out.println("(|)         ||");
+                        System.out.println("( )         ||");
+                    } else if (guesses == 1) {
+                        System.out.println(" O          ||");
+                        System.out.println("(|)         ||");
+                        System.out.println("  )         ||");
+                    } else if (guesses == 2) {
+                        System.out.println(" O          ||");
+                        System.out.println("(|)         ||");
+                        System.out.println("            ||");
+                    } else if (guesses == 3) {
+                        System.out.println(" O          ||");
+                        System.out.println(" |)         ||");
+                        System.out.println("            ||");
+                    } else if (guesses == 4) {
+                        System.out.println(" O          ||");
+                        System.out.println(" |          ||");
+                        System.out.println("            ||");
+                    } else if (guesses == 5) {
+                        System.out.println(" O          ||");
+                        System.out.println("            ||");
+                        System.out.println("            ||");
+                    }
                     System.out.println("            ||");
-                } else if (guesses == 5) {
-                    System.out.println(" O          ||");
-                    System.out.println("            ||");
-                    System.out.println("            ||");
-                }
-		        System.out.println("            ||");
-		        System.out.println("          __||__");
-                
-                if (guesses == 6) {
-                    System.out.println("You lose");
+                    System.out.println("          __||__");
+
+                    if (guesses == 6) {
+                        System.out.println("You lose");
+                        System.out.println("The word was " + theWord);
+                    }
+                } else if (theWordToGuess.equals(theWord)) {
+                    System.out.println("You win");
                     System.out.println("The word was " + theWord);
+                    break;
+                } else {
+                    System.out.println(theWordToGuess);
                 }
-            } else if (theWordToGuess.equals(theWord)) {
-                System.out.println("You win");
-                System.out.println("The word was " + theWord);
-                break;
-            } else {
-                System.out.println(theWordToGuess);
             }
-
-            input.close();
+        } catch (IOException InputMismatchException) { // catches if the file is not found
+            System.out.println("Please input a letter");
         }
-
-        inFS.close();
     }
 }
