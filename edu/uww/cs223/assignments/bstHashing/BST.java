9482596185762699fd035f065567e0248458e5b0
Cooper Recob <crecob1@gmail.com>
2023-11-09 18:32:26 +0000

    assignment 5 for cs223

--- edu/uww/cs223/assignments/bstHashing/BST.java
+++ edu/uww/cs223/assignments/bstHashing/BST.java
@@ -10,28 +10,138 @@ public class BST {
 	// END DO NOT MODIFY
 
 	public Node search(int key) {
-		
-		throw new UnsupportedOperationException("search");
+		try {
+			if (root == null) {
+				return null;
+			}
+			Node temp = root;
+			while (temp != null) {
+				if (key == temp.value) {
+					return temp;
+				} else if (key < temp.value) {
+					temp = temp.left;
+				} else {
+					temp = temp.right;
+				}
+			}
+			return null;
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("search");
+		}
 	}
 
 	public Node insert(int key) {
-		throw new UnsupportedOperationException("insert");
+		try {
+			if (root == null) {
+				root = new Node(key);
+				return root;
+			}
+			Node temp = root;
+			Node parent = null;
+			while (temp != null) {
+				if (temp.value == key) {
+					return null;
+				} else if (temp.value < key) {
+					parent = temp;
+					temp = temp.right;
+				} else {
+					parent = temp;
+					temp = temp.left;
+				}
+			}
+			Node newNode = new Node(key);
+			newNode.parent = parent;
+			if (parent.value > key) {
+				parent.left = newNode;
+			} else {
+				parent.right = newNode;
+			}
+			return newNode;
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("insert");
+		}
 	}
 
 	public boolean remove(int key) {
-		throw new UnsupportedOperationException("remove");
+		try {
+			Node nodeToDelete = search(key);
+			if (nodeToDelete == null) {
+				return false;
+			}
+			if (nodeToDelete.left != null && nodeToDelete.right != null) {
+				Node max = findMax(nodeToDelete.left);
+				nodeToDelete.value = max.value;
+				nodeToDelete = max;
+			}
+			if (nodeToDelete.left == null && nodeToDelete.right == null) {
+				removeLeaf(nodeToDelete);
+			} else {
+				removeOneChild(nodeToDelete);
+			}
+			return true;
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("remove");
+		}
 	}
 
 	private Node findMax(Node node) {
-		throw new UnsupportedOperationException("findMax");
+		try {
+			if (node == null) {
+				return null;
+			}
+			while (node.right != null) {
+				node = node.right;
+			}
+			return node;
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("findMax");
+		}
 	}
 
 	private void removeLeaf(Node leaf) {
-		throw new UnsupportedOperationException("removeLeaf");
+		try {
+			if (leaf == null) {
+				root = null;
+			} else {
+				Node parent = leaf.parent;
+				if (parent.left == leaf) {
+					parent.left = null;
+				} else {
+					parent.right = null;
+				}
+				leaf.parent = null;
+			}
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("removeLeaf");
+		}
 	}
-
+	
 	private void removeOneChild(Node node) {
-		throw new UnsupportedOperationException("removeOneChild");
+		try {
+			Node child;
+			if (node.left != null) {
+				child = node.left;
+				node.left = null;
+			} else {
+				child = node.right;
+				node.right = null;
+			}
+			if (node == root) {
+				root = child;
+				child.parent = null;
+			} else {
+				Node parent = node.parent;
+				if (parent.left == node) {
+					parent.left = child;
+				} else {
+					parent.right = child;
+				}
+				child.parent = parent;
+				node.parent = null;
+			}
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("removeOneChild");
+		}
 	}
 
 	// DO NOT MODIFY
--- edu/uww/cs223/assignments/bstHashing/Hashtable.java
+++ edu/uww/cs223/assignments/bstHashing/Hashtable.java
@@ -22,23 +22,88 @@ public class Hashtable {
 	// END DO NOT MODIFY
 
 	private int computeHash(int key) {
-		throw new UnsupportedOperationException("computeHash");
+		try {
+			return (37 * key + 61) % tableSize;
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("computeHash");
+		}
 	}
 
 	public boolean insert(int key) {
-		throw new UnsupportedOperationException("insert");
+		try {
+			if (search(key)) {
+				return false;
+			}
+			int hash = computeHash(key);
+			LinkedList<Integer> list = table.get(hash);
+			if (list == null) {
+				list = new LinkedList<>();
+				table.set(hash, list);
+			}
+			list.add(key);
+			count++;
+			return true;
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("insert");
+		}
 	}
 
 	public boolean remove(int key) {
-		throw new UnsupportedOperationException("remove");
+		try {
+			int hash = computeHash(key);
+			LinkedList<Integer> list = table.get(hash);
+			if (list == null) {
+				return false;
+			}
+			for (int i = 0; i < list.size(); i++) {
+				if (list.get(i) == key) {
+					list.remove(i);
+					count--;
+					return true;
+				}
+			}
+			return false;
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("remove");
+		}
 	}
 
 	public boolean search(int key) {
-		throw new UnsupportedOperationException("search");
+		try {
+			int hash = computeHash(key);
+			LinkedList<Integer> list = table.get(hash);
+			if (list == null) {
+				return false;
+			}
+			for (int i = 0; i < list.size(); i++) {
+				if (list.get(i) == key) {
+					return true;
+				}
+			}
+			return false;
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("search");
+		}
 	}
 
 	@Override
 	public String toString() {
-		throw new UnsupportedOperationException("toString");
+		try {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < tableSize; i++) {
+				sb.append("Slot " + i + ": ");
+				LinkedList<Integer> list = table.get(i);
+				if (list == null) {
+					continue;
+				}
+				for (int j = 0; j < list.size(); j++) {
+					sb.append(list.get(j) + " ");
+				}
+				sb.append("\n");
+			}
+			return sb.toString();
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("toString");
+		}
 	}
 }
