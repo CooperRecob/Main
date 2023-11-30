1b4e0d9c552ead64c74150c29a36299066e1c44c
Cooper Recob <crecob1@gmail.com>
2023-11-30 19:28:22 +0000

    finished assignment 6 for cs223

--- edu/uww/cs223/assignments/dijkstrasAlg/Assignment6.java
+++ edu/uww/cs223/assignments/dijkstrasAlg/Assignment6.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 public class Assignment6 {
 	// DO NOT MODIFY
 	private static final double[][] adjMatrix = new double[51][51];
@@ -39,14 +44,82 @@ public class Assignment6 {
 	// END DO NOT MODIFY
 
 	private static void fileToAdjacencyMatrix(String path) {
-		throw new UnsupportedOperationException("fileToAdjacencyMatrix");
+		try {
+			Scanner scanner = new Scanner(new File(path));
+			scanner.nextLine();
+			for (int i = 0; i < 51; i++) {
+				String[] line = scanner.nextLine().split(",");
+				cities[i] = line[0];
+				for (int j = 0; j < 51; j++) {
+					adjMatrix[i][j] = Double.parseDouble(line[j + 1]);
+				}
+			}
+			scanner.close();
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("fileToAdjacencyMatrix");
+		}
 	}
 
 	private static int cityToIndex(String city) {
+		for (int i = 0; i < 51; i++) {
+			if (cities[i].equals(city)) {
+				return i;
+			}
+		}
 		throw new UnsupportedOperationException("cityToIndex");
 	}
 
 	private static void dijkstra(int s, int t) {
-		throw new UnsupportedOperationException("dijkstra");
+		try {
+			double dist[] = new double[51];
+			for (int i = 0; i < 51; i++) {
+				dist[i] = Double.POSITIVE_INFINITY;
+			}
+			dist[s] = 0;
+			PriorityQueue<Integer> open = new PriorityQueue<Integer>((a, b) -> Double.compare(dist[a], dist[b]));
+			open.add(s);
+			boolean closed[] = new boolean[51];
+			int parent[] = new int[51];
+			for (int i = 0; i < 51; i++) {
+				parent[i] = -1;
+			}
+			while (!open.isEmpty()) {
+				int umin = open.poll();
+				closed[umin] = true;
+				for (int v = 0; v < 51; v++) {
+					if (adjMatrix[umin][v] == -1 || closed[v]) {
+						continue;
+					}
+					double len = dist[umin] + adjMatrix[umin][v];
+					if (len < dist[v]) {
+						dist[v] = len;
+						parent[v] = umin;
+					}
+					if (!open.contains(v)) {
+						open.add(v);
+					}
+				}
+			}
+			if (dist[t] == Double.POSITIVE_INFINITY) {
+				System.out.println("No path from " + cities[s] + " to " + cities[t]);
+				return;
+			}
+			int curr = t;
+			ArrayList<Integer> path = new ArrayList<Integer>();
+			while (curr != -1) {
+				path.add(curr);
+				curr = parent[curr];
+			}
+			System.out.println("Path from " + cities[s] + " to " + cities[t] + ":");
+			for (int i = path.size() - 1; i >= 0; i--) {
+				System.out.print("\t" + cities[path.get(i)]);
+				if (i != 0) {
+					System.out.print("\n");
+				}
+			}
+			System.out.println("\nTotal Distance: " + dist[t] + " miles");
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("dijkstra");
+		}
 	}
 }
